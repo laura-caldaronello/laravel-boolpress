6101abbe3d525f1f00c84be30d1641f4d3b14d8a
laura-caldaronello <caldaronello.l@gmail.com>
2021-05-19 15:18:35 +0200

    possibilità di popolare tabella posts

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $new_post = new Post();
+            $new_post->title = $faker->sentence(rand(4,10));
+            $new_post->text = $faker->text(rand(100,200));
+            $new_post->slug = Str::slug($new_post->title,'-');
+            $new_post->save();
+        }
+    }
+}
